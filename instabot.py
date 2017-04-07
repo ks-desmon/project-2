2bdc91d3769fd23a7c19dd752bd95745aabd08e3
ks rawat <ks rawat>
2017-04-07 12:03:36 +0530

    all mandaty completed with modification rmoving buggs in userchoice

--- instabot.py
+++ instabot.py
@@ -1,12 +1,11 @@
 import requests
-#access token for performing operation on user and owner account
+#access token for performing operation on user and owner account<<<<< used in the scope of basic, public_content, likes, comments.>>>>>
 tokken_for_access_app = "1993495056.1b2b25a.bbdc1be8c2364ab181433835f5c37520"
 #url part commen in all urls
 BASE_URL = "https://api.instagram.com/v1/"
 
 
-def funn_is_successful_or_not(sucess):
-    print sucess
+def funn_is_successful_or_not(sucess):#is is going to check and tell the operation performed is successful or not
     if sucess == 200:
         print "successsfully done "
     else:
@@ -30,8 +29,12 @@ def get_user_by_username(user_name):
     print user_info["data"][0]["full_name"]
     print user_info["data"][0]["username"]
     return user_info["data"][0]["id"]
-    #sucess = user_info["meta"]["code"]
-    #funn_is_successful_or_not(sucess)
+    sucess = user_info["meta"]["code"]
+    if sucess == 200:
+        print "successsfully found user id "
+    else:
+        print "unsucsessfull plz check user name again"
+
 
 #this funn is::: returning the post id :::on the bases of funn << get_user_by_user_name >>
 def get_user_post_id(username,post_number):
@@ -40,7 +43,12 @@ def get_user_post_id(username,post_number):
     url_user1=BASE_URL+"users/"+user_id1+"/media/recent/?access_token="+tokken_for_access_app#https://api.instagram.com/v1/users/{user-id}/media/recent/?access_token=ACCESS-TOKEN
     request_for_user_to_get_all_post=requests.get(url_user1).json()
     sucess = request_for_user_to_get_all_post["meta"]["code"]
+    if sucess == 200:
+        print "successsfully found user id "
+    else:
+        print "unsucsessfull plz check user name again"
     return request_for_user_to_get_all_post["data"][i]['id']
+
 
 #this funn is use to do like on users post_id which is getting from  funn <<<user_post_id>>>
 def like_on_user_post_id(user_id,post_number):
@@ -49,31 +57,32 @@ def like_on_user_post_id(user_id,post_number):
     url_post_like= BASE_URL+"media/"+user_current_post_id+"/likes"
     like_result=requests.post(url_post_like,Access_token).json()
     sucess = like_result["meta"]["code"]
-    funn_is_successful_or_not(sucess)
+    funn_is_successful_or_not(sucess)# checking is that operation is successfl or not..
 
 
 #this funn is use to comment on users post_id which is getting from  funn <<<user_post_id>>>
 def comment_on_user_id(user_id,entered_comment,post_number):
-    user_current_post_id=get_user_post_id(user_id,post_number)
+    user_current_post_id=get_user_post_id(user_id,post_number)# post_id which is getting from  funn <<<user_post_id>>>
     url_post_comment = BASE_URL + "media/" + user_current_post_id + "/comments"
     Access_token_Plus_comment ={'access_token':tokken_for_access_app,'text':entered_comment}
     comment_result=requests.post(url_post_comment,Access_token_Plus_comment).json()
     sucess = comment_result["meta"]["code"]
-    funn_is_successful_or_not(sucess)
+    funn_is_successful_or_not(sucess)# checking is that operation is successfl or not..
+
 def fun_main():
     info_owner()
     Variable1 ="y"
     while Variable1=="y" :
-        list = [ "amritbirsingh345","yashika3990"]  #List of user added by the owner
+        list = [ "amritbirsingh345","yashika3990"]  #List of all users added by the owner
         print "hey enter user name from following : amritbirsingh345 : yashika3990 "
         user_name= raw_input()
-        if user_name in list:
+        if user_name in list:                       #checking entered user is valid or not..
             print "what u want to do : for comment press 1: for like press 2"
             input = raw_input()
             print input
             if input =="1":
-                print "we have latest five posts of user on which you want to comment ?? 1 , 2 , 3 , 4 , 5"
-                list2=["1","2","3","4","5"]
+                print "we have latest five posts of user on which you want to comment ?? 0 , 1 , 2 , 3 , 4"
+                list2=["0","1","2","3","4"]         #giving choice to user to chose between no of choice
                 post_number= raw_input()
                 if post_number in list2:
                     print "write comment u want to write \n but rules are \n 1. The total length of the comment cannot exceed 300 characters.\n 2. The comment cannot contain more than 4 hashtags.\n 3. The comment cannot contain more than 1 URL.\n 4. The comment cannot consist of all capital letters."
@@ -84,8 +93,7 @@ def fun_main():
                 else:
                     print "you chose wrong choice"
             elif input =="2":
-                print "we have latest five posts of user on which you want to like ?? 1 , 2 , 3 , 4 , 5"
-                list2 = ["1", "2", "3", "4", "5"]
+                print "we have latest five posts of user on which you want to like ?? 0 , 1 , 2 , 3 , 4"
                 post_number = raw_input()
                 if post_number in list2:
                     like_on_user_post_id(user_name,post_number)
